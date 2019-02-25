91c9e3d03c0b372cf73a6a04ed2fb780018172aa
Elizabeth Le <elizabethlemk@yahoo.com>
2019-02-25 17:22:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+
+function heyJude(verse) {
+  return verse.repeat(16);
+}
+
+function prohibitedLanguage(string) {
+  const bannedWords = /candycorn|brusselssprouts/;
+  return string.search(bannedWords)
+}
