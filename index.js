ef37baadb7e037dd7c80c245d086d7b08868fd50
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-25 15:55:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+const heyJude = () => {
+    const str ="Na na na na na na na, na na na na, hey Jude."
+    return str.repeat(16);
+}
+
+const prohibitedLanguage = (str) => {
+    let myRegExp= /candycorn|brusselssprouts/
+   return str.search(myRegExp)
+}
