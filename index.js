a52049ee3ecc6276e220ae8dd52d6d7d461fb929
sbrij001 <sbrij001@yahoo.com>
2019-06-14 14:46:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function heyJude(){
+  return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+let prohibitedLanguage = (string) => {
+  let bannedWords = /candycorn|brusselssprouts/
+  return string.search(bannedWords)
+}
